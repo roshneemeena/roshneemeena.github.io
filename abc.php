c20fa54d7978745f86da627576ec578efb691013
roshneemeena <33551145+roshneemeena@users.noreply.github.com>
2018-06-05 11:09:11 +0200

    resume_1

---
+++ abc.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" a href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/font.css">
+    <link rel="stylesheet" href="css/myStyle.css">
+    
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/proper.js"></script>
+    <title>Basic Bootstrap</title>
+    </head>
+    <body>
+        <?php include('contact.php'); ?>
+       
+        <nav class="navbar navbar-default navbar-static-top">
+    <div class="container-fluid" id ="contain" >
+        <button type="button" class="navbar-toggle"
+        data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle Navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        </button>
+    <a class="navbar-brand" >Roshnee Meena PERUMAL </a> 
+    <div class="navbar-collapse collapse" >
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Home</a> </li>
+          <li><a href="#experience">Experience</a> </li>
+          <li><a href="#education">Education</a> </li>
+          <li><a href="#skills">Skills</a> </li>
+          <li><a href="#">contact</a> </li>
+        </ul>
+    </div>
+    </div>
+    </nav>
+     <div class="container-fluid" id="background">
+        <div class="row">
+            
+            <div class="col-md-6">
+
+                
+                <img src="pics/roshnee.JPG" class="img-fluid" alt="Responsive image">
+            </div>
+            <div class="col-md-6">
+                <div class="spacer">
+                
+       
+                <h4 >Roshnee Meena PERUMAL</h4>
+                <h4>Software Engineer</h4>
+                <h4>Phone</h4>
+                <p>+91 9952912776</p>
+                <h4>Email</h4>
+                <p>roshneemeena0312@gmail.com</p>
+                <h4>Address</h4>
+                <address>38,rue jean jacques rousseau,ivry sur seine</address>
+                <h4>Date of Birth</h4>
+                <p>03-Dec-1991</p>
+               
+               </div>
+            </div>
+        </div>        
+
+    </div>
+    <div class="container-fluid">
+    <div class="row" id="font">
+        <div class="col-md-4 col-md-offset-5" id="align">
+    
+            <p><strong>Hi! I'm Roshnee Meena</strong></p>
+ 
+
+            I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.
+         </div>
+    </div>
+    </div>
+
+    <div class="container-fluid" id="experience">
+        
+        
+                
+        <div class="row" id="exp">
+                <h3 class="h3">Experience</h3>
+            <div class="col-sm-3"></div>
+            <div class="col-sm-3" id="year"> <p>2013-2014</p><p>Executive</p></div>
+            <div class="col-sm-3 "> <p><strong>COMPANY NAME</strong> </p><p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p></div>
+            <div class="col-sm-3"></div>
+    </div>
+    
+    <div class="row" id="exp">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-3" id="year"> <p>2014-2016</p><p>Executive</p></div>
+            <div class="col-sm-3 "> <p> <strong>COMPANY NAME</strong></p><p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p></div>
+            <div class="col-sm-3"></div>
+    </div>
+
+   </div>
+
+   <div class="container-fluid" id="education">
+        <div class="row" id="edu">
+        <h3 class="h3">Education</h3>
+        <div class="col-sm-3"></div>
+            <div class="col-sm-3" > <p>2017-2019</p><p>Master's Degree</p></div>
+            <div class="col-sm-3 "> <p><strong>EPITA</strong> </p><p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p></div>
+            <div class="col-sm-3"></div>
+            </div>
+        <div class="row" id="exp">
+             <div class="col-sm-3"></div>
+             <div class="col-sm-3" id="year"> <p>2009-2013</p><p>Bachlors's Degree</p></div>
+             <div class="col-sm-3 "> <p> <strong>Anna University</strong></p><p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p></div>
+             <div class="col-sm-3"></div>
+        </div>
+        <div class="row" id="exp">
+                <div class="col-sm-3"></div>
+                <div class="col-sm-3" id="year"> <p>2007-2009</p><p>High School</p></div>
+                <div class="col-sm-3 "> <p> <strong>Adarsh Vidya kendra</strong></p><p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p></div>
+                <div class="col-sm-3"></div>
+        </div>
+
+
+
+
+   </div>
+   
+
+   
+<div class="container-fluid" id="skills">
+        <h3 class="h2">Skills</h3>        
+
+ <div id="grid">
+        <div id="item1">Microsoft ExCel - Advanced</div>
+        <div id="item2">Microsoft ExCel - Advanced</div>
+        <div id="item3">Microsoft ExCel - Advanced</div>
+        <div id="item3">Microsoft ExCel - Advanced</div>
+        <div id="item3">Microsoft ExCel - Advanced</div>
+        <div id="item3">Microsoft ExCel - Advanced</div>
+        <div id="item3">Microsoft ExCel - Advanced</div>
+        <div id="item3">Microsoft ExCel - Advanced</div>
+      </div>
+      </div>
+ 
+    <a href="basics.html"></a>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+                <h3>Contact form</h3>
+                <form id="contact-form" method="POST" action="<?=$_SERVER['PHP_SELF']; ?>" role="form">
+                      <div class="message"></div>
+                      <div class="controls">
+                          <div class="row">
+                              <div class="col-md-6">
+                                  <div class="form-group">
+                                      <label for="form_name">NAME</label>
+                                      <input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your name *">
+                                      <div class="help-block with-errors"></div>
+                                      
+
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                    <label for="form_email">Email</label>
+                                    <input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your phone number">
+                                    <div class="help-block with-errors"></div>
+                                 </div>
+                                </div>
+                            </div>
+                       <div class="row">
+                           <div class="col-md-6">
+                               <div class="form-group">
+                               <label for="form_phone">Phone</label>
+                               <input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Please enter your phone number">
+                               <div class="help-block with-errors"></div>
+                            </div>
+                           </div>
+                       </div>
+
+                       <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="form_message">Message *</label>
+                                    <textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please,leave us a message."></textarea>
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <input type="submit" class="btn btn-success btn-send" value="Send message">
+
+                            </div>
+                       </div>
+                       <div class="row">
+                           <div class="col-md-12">
+                               <p class="text-muted"><strong>*</strong> These fields are mandatory</p>
+                            
+                       </div>
+                       </div>
+                          
+              
+                  </form
+                   
+                   
+              
+                
+        </div>
+
+    </div>
+</div>
+  
---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" a href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/font.css">
+    <link rel="stylesheet" href="css/bootstrap-responsive.css">
+    <link rel="stylesheet" href="css/myStyle.css">
+    
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/proper.js"></script>
+    <title>Basic Bootstrap</title>
+    </head>
+    <body>
+        
+       <?php include('contact.php'); ?>
+       
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container-fluid" id ="contain" >
+        <div class="navbar-header">
+        <button type="button" class="navbar-toggle"
+        data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle Navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        </button>
+    <a class="navbar-brand" >Roshnee Meena PERUMAL </a> 
+        </div>
+    <div class="navbar-collapse collapse" >
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#home">Home</a> </li>
+           <li><a href="#about">About</a> </li>
+          <li><a href="#experience">Experience</a> </li>
+          <li><a href="#education">Education</a> </li>
+          <li><a href="#skills">Skills</a> </li>
+          <li><a href="#contact-form">contact</a> </li>
+        </ul>
+    </div>
+    </div>
+    </nav>
+        
+     <div class="container-fluid" id="background">
+        <div class="row" id="home">
+            
+            <div class="col-xs-6">
+
+                
+                <img src="pics/final.JPG" class="img-fluid" alt="Responsive image">
+            </div>
+            <div class="col-xs-6" id ="spacer">
+                
+                
+               
+                <h4 >Roshnee Meena PERUMAL</h4>
+                <h4>Software Engineer</h4>
+                <h4>Phone</h4>
+                <p>+33 613208562</p>
+                <h4>Email</h4>
+                <p>roshneemeena0312@gmail.com</p>
+                <h4>Address</h4>
+                <address>38,rue jean jacques rousseau,ivry sur seine</address>
+                <h4>Date of Birth</h4>
+                <p>03-Dec-1991</p>
+               
+              
+            </div>
+        </div>        
+
+    </div>
+        
+        
+    <div class="container-fluid" id="about">
+    <div class="row" id="align">
+        <div class="col-xs-4"></div>
+        <div class ="col-xs-4">
+       
+        
+    
+            <p id="p"><strong>Hi! I'm Roshnee Meena</strong></p>
+          Doing my Master in computer Science in Software Engineering. I have 2.5 years work experience in Infosys Limited, Chennai, India.
+          I am a person who strongly believe in "IMPLEMENTATION BEATS ORATION".
+My strengths are optimism, adaptability and curiosity. I am always in
+search for new challenges, learn from them and improve my skills and
+knowledge.
+ 
+
+            
+         </div>
+    </div>
+    </div>
+    
+    <div class="container-fluid" id="experience">
+        
+        
+          <h3 id="h3">Experience</h3>      
+        <div class="row" id="exp">
+            
+                
+            <div class="col-xs-3"></div>
+            <div class="col-xs-3" id="year"> <p>2013-2014</p><p>System Engineer Trainee</p></div>
+            <div class="col-xs-3 "> <p><strong>Infosys Limited,Mysore</strong> </p><p>I had a training in "Application development and Maintance in java" for 5 months.</p></div>
+            <div class="col-xs-3"></div>
+    </div>
+    
+    <div class="row" id="exp">
+            <div class="col-xs-3"></div>
+            <div class="col-xs-3" id="year"> <p>2014-2016</p><p>Senior System Engineer</p></div>
+            <div class="col-xs-3 "> <p> <strong>Infosys Limited, Chennai</strong></p><p>I worked in GIS domain for a US telecommunication company and I was maintaining the dataloads. The scripts for the dataloads were developed in oracle and unix platform. The Map services were developed using ARcGIS tools.
+                                                                                         I was awarded with BRAVO award for the outstanding performance and valuble contribution and Insta award for the Suceessful completion of the projects.<p></div>
+            <div class="col-xs-3"></div>
+    </div>
+
+   </div>
+
+   <div class="container-fluid" id="education">
+       <h3 id="h3">Education</h3>
+        <div class="row" id="exp">
+        
+        <div class="col-xs-3"></div>
+            <div class="col-xs-3" > <p>2017-2019</p><p>Master's Degree</p></div>
+            <div class="col-xs-3 "> <p><strong>EPITA, Paris</strong> </p><p>I am doing my masters degree in MAsters of Computer Science in Software Engineering</p></div>
+            <div class="col-xs-3"></div>
+            </div>
+        <div class="row" id="exp">
+             <div class="col-xs-3"></div>
+             <div class="col-xs-3" id="year"> <p>2009-2013</p><p>Bachlors's Degree</p></div>
+             <div class="col-xs-3 "> <p> <strong>Anna University, Chennai</strong></p><p>I did my Bachlors in Electronics and Instrumentation in Pnimala Engineering college Under Anna university</p></div>
+             <div class="col-xs-3"></div>
+        </div>
+     
+
+
+
+   </div>
+   
+
+   
+<div class="container-fluid" id="skills">
+        <h3 id="h3">Skills</h3>     
+        
+
+        <div class="row" id="grid">
+        <div class="col-xs-12" id="item3">JAVA</div>
+        <div class="col-xs-12" id="item3">Spring Frame work</div>
+        <div class="col-xs-12" id="item3">Hibernate Frame Work</div>
+        <div class="col-xs-12" id="item3">Java Script</div>
+        </div>
+         <div class="row" id="grid">
+        <div class="col-xs-12" id="item3">PHP</div>
+        <div class="col-xs-12" id="item3">CSS</div>
+        <div class="col-xs-12" id="item3">HTML</div>
+        <div class="col-xs-12" id="item3">Bootstrap Framework</div>
+        </div>
+         <div class="row" id="grid">
+        <div class="col-xs-12" id="item3">IOS development</div>
+        <div class="col-xs-12" id="item3">UNIX</div>
+        <div class="col-xs-12" id="item3">Angular JS</div>
+        <div class="col-xs-12" id="item3">ArcGIS</div>
+        </div>
+      </div>
+      </div>
+        
+ 
+   
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+                <h3>Contact form</h3>
+                <form id="contact-form" method="POST" action="<?=$_SERVER['PHP_SELF']; ?>" role="form">
+                      <div class="message"></div>
+                      <div class="controls">
+                          <div class="row">
+                              <div class="col-md-6">
+                                  <div class="form-group">
+                                      <label for="form_name">NAME</label>
+                                      <input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your name *">
+                                      <div class="help-block with-errors"></div>
+                                      
+
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                    <label for="form_email">Email</label>
+                                    <input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your phone number">
+                                    <div class="help-block with-errors"></div>
+                                 </div>
+                                </div>
+                            </div>
+                       <div class="row">
+                           <div class="col-md-6">
+                               <div class="form-group">
+                               <label for="form_phone">Phone</label>
+                               <input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Please enter your phone number">
+                               <div class="help-block with-errors"></div>
+                            </div>
+                           </div>
+                       </div>
+
+                       <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="form_message">Message *</label>
+                                    <textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please,leave us a message."></textarea>
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <input type="submit" class="btn btn-success btn-send" value="Send message">
+
+                            </div>
+                       </div>
+                       <div class="row">
+                           <div class="col-md-12">
+                               <p class="text-muted"><strong>*</strong> These fields are mandatory</p>
+                            
+                       </div>
+                       </div>
+                          
+              
+                  </form
+                   
+                   
+              
+                
+        </div>
+
+    </div>
+</div>
+  
